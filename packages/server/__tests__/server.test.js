b66c5b27cf9cf05ddfe3c477bf2acd5592b5ebab
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-02-23 10:57:07 +0000

    use healthchek router

--- packages/server/__tests__/server.test.js
+++ packages/server/__tests__/server.test.js
@@ -1,9 +1,19 @@
 const express = require('express')
+
+const mockHealthcheckRouter = () => {}
+
+jest.mock('../lib/routers/healthcheck', () => mockHealthcheckRouter)
+
 let server
 
 beforeAll(() => {
   express.application.listen = jest.fn()
+  express.application.use = jest.fn()
   server = require('../lib/server')
+})
+
+it('uses the healthcheck router on /healthcheck', () => {
+  expect(express.application.use).toHaveBeenCalledWith('/healthcheck', mockHealthcheckRouter)
 })
 
 describe('start', () => {
--- packages/server/lib/server.js
+++ packages/server/lib/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use('/healthcheck', require('./routers/healthcheck'))
+
 module.exports.start = ({ port }) => {
   app.listen(port)
 }
