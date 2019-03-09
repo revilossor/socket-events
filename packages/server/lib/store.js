60a99aaf7245559234970d5e29b861105f522f81
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-03-09 14:39:22 +0000

    less magic numbers

--- packages/server/lib/store.js
+++ packages/server/lib/store.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose')
 
+const connectionRetryInterval = 1000
+
 let connectionUri
 
 mongoose.Promise = global.Promise
 const db = mongoose.connection
 
+const connect = () => mongoose.connect(connectionUri, { useNewUrlParser: true })
+
 db.on('error', () => {
   mongoose.disconnect()
 })
 db.on('disconnected', () => {
-  setTimeout(() => {
-    mongoose.connect(connectionUri, { useNewUrlParser: true })
-  }, 1000)
+  setTimeout(connect, connectionRetryInterval)
 })
 
 module.exports.connect = uri => new Promise((resolve, reject) => {
   connectionUri = uri
-  mongoose.connect(connectionUri, { useNewUrlParser: true })
+  connect()
   db.once('open', () => {
     console.log('connection opened!')
     resolve()
