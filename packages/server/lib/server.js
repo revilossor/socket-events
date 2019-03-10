616709978111a35e1d5bc7b801731dffe15d055e
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-03-10 15:11:18 +0000

    init socket io

--- packages/server/lib/server.js
+++ packages/server/lib/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 app.use('/healthcheck', require('./routers/healthcheck'))
 app.use('/event', require('./routers/event'))
 
+// console.dir({
+//   server,
+//   io
+// })
+
+io.on('connection', () => {
+  console.log('user connected!')
+})
+
 module.exports.start = ({ port }) => {
-  app.listen(port)
+  server.listen(port)
 }
--- packages/server/tests/server.test.js
+++ packages/server/tests/server.test.js
@@ -3,23 +3,59 @@ const express = require('express')
 const mockHealthcheckRouter = () => {}
 const mockEventRouter = () => {}
 
-jest.mock('../lib/routers/healthcheck', () => mockHealthcheckRouter)
-jest.mock('../lib/routers/event', () => mockEventRouter)
+const mockServer = {
+  listen: jest.fn()
+}
+const mockHttp = {
+  createServer: jest.fn(() => mockServer)
+}
+const mockOnHandlers = {}
+const mockIo = {
+  on: jest.fn((key, func) => { mockOnHandlers[key] = func })
+}
+const mockSocketIo = jest.fn(() => mockIo)
 
 let server
 
 beforeAll(() => {
+  jest.mock('http', () => mockHttp)
+  jest.mock('socket.io', () => mockSocketIo)
+  jest.mock('../lib/routers/healthcheck', () => mockHealthcheckRouter)
+  jest.mock('../lib/routers/event', () => mockEventRouter)
   express.application.listen = jest.fn()
   express.application.use = jest.fn()
   server = require('../lib/server')
 })
 
-it('uses the healthcheck router on /healthcheck', () => {
-  expect(express.application.use).toHaveBeenCalledWith('/healthcheck', mockHealthcheckRouter)
+describe('express app', () => {
+  it('uses the healthcheck router on /healthcheck', () => {
+    expect(express.application.use).toHaveBeenCalledWith('/healthcheck', mockHealthcheckRouter)
+  })
+
+  it('uses the event router on /event', () => {
+    expect(express.application.use).toHaveBeenCalledWith('/event', mockEventRouter)
+  })
 })
 
-it('uses the event router on /event', () => {
-  expect(express.application.use).toHaveBeenCalledWith('/event', mockEventRouter)
+describe('socket.io', () => {
+  it('creates an http server', () => {
+    expect(mockHttp.createServer).toHaveBeenCalled()
+  })
+  it('creates an io instance', () => {
+    expect(mockSocketIo).toHaveBeenCalledWith(mockServer)
+  })
+  it('listens for connections', () => {
+    expect(mockIo.on).toHaveBeenCalledWith('connection', expect.any(Function))
+  })
+  describe('on client connection', () => {
+    beforeAll(() => {
+      console.log = jest.fn()
+      mockOnHandlers.connection()
+    })
+    it('logs', () => {
+      expect(console.log).toHaveBeenCalledWith('user connected!')
+    })
+  })
 })
 
 describe('start', () => {
@@ -31,7 +67,7 @@ describe('start', () => {
     server.start(options)
   })
 
-  it('listens on the port specified', () => {
-    expect(express.application.listen).toHaveBeenCalledWith(options.port)
+  it('http server listens on the port specified', () => {
+    expect(mockServer.listen).toHaveBeenCalledWith(options.port)
   })
 })
