5849577c623827cb04b38c90a7071a352e7446e5
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-03-11 21:04:42 +0000

    Agrgregate update state from event handler

--- packages/aggregate/babel.config.js
+++ packages/aggregate/babel.config.js
@@ -1,7 +1,9 @@
 module.exports = function (api) {
   api.cache(false)
   const presets = ['@babel/env']
+  const plugins = ['@babel/plugin-transform-runtime']
   return {
-    presets
+    presets,
+    plugins
   }
 }
