9af6def981afa0447811f7221d931e5c9cf041eb
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-03-11 11:37:29 +0000

    mock server in index tests

--- packages/server/tests/index.test.js
+++ packages/server/tests/index.test.js
@@ -1,13 +1,13 @@
-const server = require('../lib/server')
-
-jest.mock('../lib/server')
+const mockServer = {
+  start: jest.fn()
+}
+jest.mock('../lib/server', () => mockServer)
 
 const uri = 'mongodb://store:27017/events' // TODO envs for config?
 
 const mockStore = {
   connect: jest.fn(() => Promise.resolve())
 }
-
 jest.mock('../lib/store', () => mockStore)
 
 beforeAll(() => {
@@ -22,7 +22,7 @@ describe('connects to the store', () => {
 
 describe('starts the server', () => {
   it(`"port" is "8080"`, () => {
-    expect(server.start).toHaveBeenCalledWith(
+    expect(mockServer.start).toHaveBeenCalledWith(
       expect.objectContaining({ port: 8080 })
     )
   })
