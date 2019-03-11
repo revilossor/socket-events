4b2fbf3760d086d6b40c77c813d54ffd374431bf
Oliver Ross <oliver.ross01@bbc.co.uk>
2019-03-11 21:02:22 +0000

    Agrgregate update state from event handler

--- packages/aggregate/src/Aggregate.js
+++ packages/aggregate/src/Aggregate.js
@@ -6,6 +6,21 @@ class Aggregate {
     this.id = id
     this.socket = io(url)
     this.state = null
+    this.handlers = {}
+    this.version = 0
+  }
+  register (event, handler) {
+    this.handlers[event] = handler
+  }
+  deregister (event) {
+    delete this.handlers[event]
+  }
+  async push (item) {
+    if (!this.handlers[item.event]) {
+      throw Error(`cannot handle ${item.event}`)
+    } else {
+      this.state = this.handlers[item.event].call(this, this.state, item.data)
+    }
   }
 }
 
--- packages/aggregate/tests/Aggregate.test.js
+++ packages/aggregate/tests/Aggregate.test.js
@@ -5,17 +5,19 @@ const mockSocket = {
 }
 const mockIo = jest.fn(() => mockSocket)
 
+const url = 'mock url'
+const id = 'mock id'
+const event = 'mock event'
+const data = 'mock data'
+
+let instance
+
 beforeAll(() => {
   jest.mock('socket.io-client', () => mockIo)
   Aggregate = require('../src/Aggregate').default
 })
 
 describe('constructor', () => {
-  const url = 'mock url'
-  const id = 'mock id'
-
-  let instance
-
   beforeAll(() => {
     instance = new Aggregate(url, id)
   })
@@ -40,16 +42,114 @@ describe('constructor', () => {
   it('inits state to null', () => {
     expect(instance.state).toBe(null)
   })
+
+  it('inits version to null', () => {
+    expect(instance.version).toEqual(0)
+  })
+
+  it('inits handlers to an empty object', () => {
+    expect(instance.handlers).toEqual({})
+  })
 })
 
-// describe('register', () => {
-//   // register an event handler
-// })
-//
-// describe('push', () => {
-//   // adds events
-// })
-//
+describe('register', () => {
+  beforeAll(() => {
+    instance = new Aggregate(url, id)
+  })
+
+  it('adds the handler to the instance', () => {
+    expect(instance.handlers[event]).not.toBeDefined()
+    instance.register(event, () => {})
+    expect(instance.handlers[event]).toBeDefined()
+  })
+})
+
+describe('deregister', () => {
+  beforeAll(() => {
+    instance = new Aggregate(url, id)
+    instance.register(event, () => {})
+  })
+
+  it('removed the handler from the instance', () => {
+    expect(instance.handlers[event]).toBeDefined()
+    instance.deregister(event)
+    expect(instance.handlers[event]).not.toBeDefined()
+  })
+})
+
+describe('push', () => {
+  const mockEvent = {
+    event,
+    data
+  }
+
+  describe('if there is a handler for the event', () => {
+    const handler = jest.fn((state, data) => `${state} =-> ${data}`)
+
+    let before
+    let after
+
+    beforeAll(() => {
+      instance = new Aggregate(url, id)
+    })
+
+    beforeAll(async () => {
+      instance.register(event, handler)
+      before = instance.state
+      await instance.push(mockEvent)
+      after = instance.state
+    })
+
+    afterAll(() => {
+      instance.derigester(event)
+    })
+
+    describe('calls the handler', () => {
+      it('with the state', () => {
+        expect(handler.mock.calls[0][0]).toEqual(before)
+      })
+      it('with the event data', () => {
+        expect(handler.mock.calls[0][1]).toEqual(data)
+      })
+    })
+
+    it('updates the state to the return value of the handler', () => {
+      expect(after).toEqual(`${before} =-> ${data}`)
+    })
+
+    // describe('upodates the event in the store', () => {
+    //   it('updates the version to the version of the pushed event', () => {
+    //     expect(after).toEqual(`${before} =-> ${data}`)
+    //   })
+    // })
+  })
+
+  describe('if there is no handler for the event', () => {
+    beforeAll(() => {
+      instance = new Aggregate(url, id)
+    })
+
+    it('doesnt change the state', async () => {
+      const before = instance.state
+      try {
+        await instance.push(mockEvent)
+      } catch (err) {
+        // do nothing
+      } finally {
+        expect(instance.state).toEqual(before)
+      }
+    })
+
+    it('rejects with an error', async () => {
+      await expect(instance.push(mockEvent)).rejects.toEqual(Error(`cannot handle ${event}`))
+    })
+  })
+
+  // it('') // TODO calls teh handler, pushes the events
+  // TODO if no handler / handler errrors
+  // TODO if error pushing
+})
+
 // describe('play', () => {
 //   // update version to latest and start listening for events
 // })
